b436c6e2f0cbfb268dec1791b0094f45ce5f885a
berkgulsen <bipurpie.exe@gmail.com>
2023-10-23 21:19:40 +0300

    Initial commit

---
+++ dotnetECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetECommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
