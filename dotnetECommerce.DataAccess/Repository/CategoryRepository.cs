e352bebed53301b387d67a36514211f7e43eeb5b
berkgulsen <bipurpie.exe@gmail.com>
2023-10-30 17:28:02 +0300

    category interface and repository added

---
+++ dotnetECommerce.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+using dotnetECommerce.DataAccess.Data;
+using dotnetECommerce.DataAccess.Repository.IRepository;
+using dotnetECommerce.Models;
+
+namespace dotnetECommerce.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private ApplicationDbContext _db;
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Update(Category obj)
+    {
+        _db.Update(obj);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+}
---
+++ dotnetECommerce.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+using dotnetECommerce.Models;
+
+namespace dotnetECommerce.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category obj);
+    void Save();
+}
