0be1904ae0c2c96389bae99c2cff016e4ace49de
berkgulsen <bipurpie.exe@gmail.com>
2023-11-03 22:41:40 +0300

    Products migration created

--- dotnetECommerce.DataAccess/Migrations/20231103193817_addProductsToDb.Designer.cs
+++ dotnetECommerce.DataAccess/Migrations/20231103193817_addProductsToDb.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotnetECommerce.DataAccess.Data;
 
@@ -10,9 +11,11 @@ using dotnetECommerce.DataAccess.Data;
 namespace dotnetECommerce.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231103193817_addProductsToDb")]
+    partial class addProductsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
